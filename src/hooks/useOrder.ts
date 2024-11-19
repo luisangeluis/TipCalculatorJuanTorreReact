db25bc8f5315a452c4644446e66da305f4ed6ca0
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2024-11-19 18:13:54 +0000

    Test

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -3,6 +3,8 @@ import { OrderItem } from '../types';
 
 const useOrder = () => {
     const [order, setOrder] = useState<OrderItem[]>([]);
+    console.log("test");
+    
 
     return {
         order
